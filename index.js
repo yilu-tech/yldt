d23226d0c21af1187a041d6aef19c63f8ea3a61a
zhouzb <zhouzb@yilu-tech.com>
2018-06-11 20:35:40 +0800

    publish 1.0.2

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+let package_json = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8');
+let version = JSON.parse(package_json).version;
+
 require('./src/caporal/command');
 
 const prog = require('caporal');
@@ -11,5 +17,6 @@ require('./src/keyManager')(prog)
 
 require('./src/wscmd')(prog)
 
-prog.version('1.0.1')
+prog.version(version)
+
 prog.parse(process.argv);
