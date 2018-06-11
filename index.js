c555a3813080e4fdf6764877fce2194f37c1dca9
zhouzb <zhouzb@yilu-tech.com>
2018-06-11 20:52:33 +0800

    添加yldt update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-
-let package_json = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8');
-let version = JSON.parse(package_json).version;
+const { exeSync } = require('child_process');
 
 require('./src/caporal/command');
 
 const prog = require('caporal');
+
+let package_json = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8');
+let version = JSON.parse(package_json).version;
+prog.version(version);
 
 require('./src/config').init();
 
@@ -17,6 +19,14 @@ require('./src/keyManager')(prog)
 
 require('./src/wscmd')(prog)
 
-prog.version(version)
+prog.command('update', '更新yldt')
+  .action(() => {
+    try {
+      let arg = { stdio: 'inherit' };
+      exeSync('npm uninstall -g yldt', arg);
+      exeSync('npm install -g yldt', arg)
+    }
+    catch (err) { console.log(err.message); }
+  });
 
 prog.parse(process.argv);
